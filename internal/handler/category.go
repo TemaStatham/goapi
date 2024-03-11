4e0d25c1740562b6ddd57b7912ec06e93809f242
Karatist <skammoshenik@gmail.com>
2024-03-11 18:48:45 +0300

    добавил хэндлеровский слой для категорий

--- internal/handler/category.go
+++ internal/handler/category.go
@@ -39,14 +39,99 @@ func (h *Handler) addCategory(c *gin.Context) {
 	})
 }
 
-func (h *Handler) deleteCategory(c *gin.Context) {
+type deleteCategory struct {
+	Name string `json:"name" binding:"required"`
+}
 
+func (h *Handler) deleteCategory(c *gin.Context) {
+	const op = "handler.deleteCategory"
+
+	log := h.log.With(
+		slog.String("op", op),
+	)
+
+	var input deleteCategory
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, InvalidInputBodyErr)
+		log.Error("error bind json:", InvalidInputBodyErr)
+		return
+	}
+
+	err := h.CategoryService.Delete(c.Request.Context(), input.Name)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		log.Error("error added category", err.Error())
+		return
+	}
+
+	log.Info("Handler category deleted")
+
+	c.JSON(http.StatusOK, "success")
+}
+
+type editCategory struct {
+	Name string `json:"name" binding:"required"`
 }
 
 func (h *Handler) editCategory(c *gin.Context) {
+	const op = "handler.editCategory"
 
+	log := h.log.With(
+		slog.String("op", op),
+	)
+
+	var input editCategory
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, InvalidInputBodyErr)
+		log.Error("error bind json:", InvalidInputBodyErr)
+		return
+	}
+
+	id, err := h.CategoryService.Edit(c.Request.Context(), input.Name)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		log.Error("error added category", err.Error())
+		return
+	}
+
+	log.Info("Handler category edited")
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"categoryID": id,
+	})
+}
+
+type getAllCategoryiesType struct {
+	Tag string `json:"tag" binding:"required"`
 }
 
 func (h *Handler) getAllCategory(c *gin.Context) {
+	const op = "handler.getAllCategoryies"
 
+	log := h.log.With(
+		slog.String("op", op),
+	)
+
+	var input getAllCategoryiesType
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, InvalidInputBodyErr)
+		log.Error("error bind json:", InvalidInputBodyErr)
+		return
+	}
+
+	categoryies, err := h.CategoryService.GetAllProducts(c.Request.Context(), input.Tag)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		log.Error("error getting all products", err.Error())
+		return
+	}
+
+	log.Info("Handler getting all categoryies")
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"categoryies": categoryies,
+	})
 }
