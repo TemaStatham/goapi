4d2d2280543612908c49200cb01fedd34a5d89f4
Karatist <skammoshenik@gmail.com>
2024-03-11 14:34:08 +0300

    добавил логирование

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"goapi/internal/config"
+	"log/slog"
+	"os"
 )
 
 // go run ./cmd/api/main.go --config="./config/config.yaml"
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.Any("cfg", cfg))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
