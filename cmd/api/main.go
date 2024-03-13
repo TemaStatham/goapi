fbae961d8ee2505d74f2286a8938cbabc7e8553c
Karatist <skammoshenik@gmail.com>
2024-03-14 00:28:45 +0300

    не докер а docker

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,7 +7,7 @@ import (
 
 // Как запустить мое приложение
 // 1. Запуск бд
-// 	Нужен докер и утилита migrate
+// 	Нужен docker и утилита migrate
 // 		docker run --name=api-db -e POSTGRES_PASSWORD='qwerty' -p 5436:5432 -d --rm postgres
 // 		migrate -path ./schema -database 'postgres://postgres:qwerty@localhost:5436/postgres?sslmode=disable' up
 //
